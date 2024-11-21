c7a1742eb7c1226ef41c12105dc314d7cf591d1b
Megood1210 <xuanvinhle06@gmail.com>
2024-11-21 14:32:20 +0700

    Upload bài tập 2

---
+++ Session3 bài 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Nhap nhiet do theo do Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9/5) + 32;
+
+    printf("%.2f do Celsius tuong duong voi %.2f do Fahrenheit.\n", celsius, fahrenheit);
+
+    return 0;
+}
+
+
