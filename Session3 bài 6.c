dafab19440eaca0d4acc34901f9dc2f075a37ba0
Megood1210 <xuanvinhle06@gmail.com>
2024-11-21 20:01:24 +0700

    Upload bài tập 6

---
+++ Session3 bài 6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a, b, dt;
+	printf("Nhap chieu cao a = ");
+	scanf("%d",&a);
+	printf("Nhap canh b = ");
+	scanf("%d",&b);
+	
+	dt= a/2 * b;
+	
+	printf("Dien tich cua tam giac = %d",dt);	
+	 
+	return 0;	
+}
